66beda2280b5e14d428f745c207cc048362360aa
Zack M Fleischman <ZackMFleischman@gmail.com>
2025-12-11 19:39:51 -0800

    chore: format import statement

--- src/game/scenes/RaceScene.ts
+++ src/game/scenes/RaceScene.ts
@@ -1,10 +1,6 @@
 import * as Phaser from 'phaser';
 import { GAME_EVENTS } from '../events';
-import type {
-  MathAnswerPayload,
-  GameState,
-  RacerResult,
-} from '../events';
+import type { MathAnswerPayload, GameState, RacerResult } from '../events';
 import rosieSpriteUrl from '../../assets/rosie-sprite.png';
 import { AudioManager, AUDIO_KEYS } from '../systems/AudioManager';
 import { getRandomRacers, type FamilyMember } from '../../data/familyMembers';
